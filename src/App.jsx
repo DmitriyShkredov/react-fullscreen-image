0d0fbe54f3a0b7feaf05f4815be44ef6466d5dc2
DmitriyShkredov <dmitriyshkredovdev@gmail.com>
2023-11-04 15:24:26 +0300

    feat: add fullpage image

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,30 @@
 import "./App.css";
+import { useRef } from "react";
 
 function App() {
-  return <></>;
+  const imgRef = useRef(null);
+
+  const handleClick = () => {
+    if (!document.fullscreenElement) {
+      imgRef.current.requestFullscreen().catch((error) => {
+        console.log(error);
+      });
+    } else {
+      document.exitFullscreen();
+    }
+  };
+
+  return (
+    <section>
+      <h2>Click me</h2>
+      <img
+        src="./images/image.jpg"
+        alt=""
+        onClick={() => handleClick()}
+        ref={imgRef}
+      />
+    </section>
+  );
 }
 
 export default App;
